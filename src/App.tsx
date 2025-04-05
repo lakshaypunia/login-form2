7e9012dd8f1651bf82bb468d530c4d4e063dfbc5
LAKSHAY PUNIA <148634500+Lakshay-punia@users.noreply.github.com>
2025-04-05 23:55:30 +0530

    build errors removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-
 import './App.css'
 import Homepage from './components/pages/homepage'
 import CreateAccount from './components/ui/create_account'
 import LoginForm from './components/ui/login-form'
-import ProfileCard from './components/pages/profilepage'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProfilePage from './components/pages/profilepage'
 
